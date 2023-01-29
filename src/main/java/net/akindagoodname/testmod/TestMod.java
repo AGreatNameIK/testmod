ac3f4d03c52af2bebadfbdcc247710418ce4ba4f
akindagoodname <andyho0602@gmail.com>
2023-01-29 10:49:27 -0500

    a

--- src/main/java/net/akindagoodname/testmod/TestMod.java
+++ src/main/java/net/akindagoodname/testmod/TestMod.java
@@ -1,9 +1,12 @@
 package net.akindagoodname.testmod;
 
 import com.mojang.logging.LogUtils;
+import net.akindagoodname.testmod.item.ModCreativeModeTabs;
 import net.akindagoodname.testmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,10 +18,10 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TestMod.MOD_ID)
 public class TestMod {
-
+    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "testmod";
+    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-
     public TestMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -27,10 +30,26 @@ public class TestMod {
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        if(event.getTab() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BLACK_OPAL);
+            event.accept(ModItems.RAW_BLACK_OPAL);
+        }
+
+        if(event.getTab() == ModCreativeModeTabs.TEST_TAB) {
+            event.accept(ModItems.BLACK_OPAL);
+            event.accept(ModItems.RAW_BLACK_OPAL);
+            event.accept(ModItems.ZIRCON);
+            event.accept(ModItems.RAW_ZIRCON);
+        }
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
@@ -38,6 +57,7 @@ public class TestMod {
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
+
         }
     }
 }
